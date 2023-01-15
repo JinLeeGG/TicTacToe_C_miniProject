2bc786ab93322ceb04b1e2d53536340d460853e5
GyuJin Lee <gjlee99211@gmail.com>
2023-01-15 05:41:42 -0500

    drawBoard() function added

--- Tic-Tac_Toe.c
+++ Tic-Tac_Toe.c
@@ -6,7 +6,7 @@
 /* Initialize the base */
 char base[10] = {"none", "1", "2", "3", "4", "5", "6", "7", "8", "9"}; //"none" is only to match the index so there is no confusion
 
-int checkWin();
+int checkWin(); //it will return 1 if there is winner, -1 if the game is in progress, and 0 if game has been drawn  
 void drawBoard();
 
 
@@ -24,7 +24,8 @@ int main(){
         player = (player % 2) ? 1 : 2; //if player's value is zero, it will return false and if its 1, it will return true
         printf("Player %d, enter the choice : ", player);
         scanf("%d", &choice);
-
+        
+        mark = (player == 1) ? 'X' : 'O'; 
 
         // if player's choice is n number, mark n 
         if(choice == 1 && base[1] == "1"){
@@ -61,12 +62,71 @@ int main(){
             player--;
             getch(); //return to the game
         }
-        i = checkWin();
+        i = checkWin(); 
         player++;
         
 
 
+
     } while (i == -1);
+
+    drawBoard();
+    if(i == 1) {
+        printf("Player %d Won!!", --player);
+    }
+    else {
+        printf("==>Game draw");
+    }
+        getch();
+        return 0;
 }
 
+/*
+Tic Tac Toe has 8 winning cases  
+*/
+int checkWin(){
+    if(base[1] == base[2] && base[2] == base[3]){
+        return 1;
+    }
+    else if(base[4] == base[5] && base[5] == base[6]){
+        return 1;
+    }
+    else if(base[7] == base[8] && base[8] == base[9]){
+        return 1;
+    } 
+    else if(base[1] == base[4] && base[4] == base[7]){
+        return 1;
+    } 
+    else if(base[2] == base[5] && base[5] == base[8]){
+        return 1;
+    } 
+    else if(base[3] == base[6] && base[6] == base[9]){
+        return 1;
+    } 
+    else if(base[1] == base[5] && base[5] == base[9]){
+        return 1;
+    } 
+    else if(base[3] == base[5] && base[5] == base[7]){
+        return 1;
+    } 
+    else if(base[1] != "1" && base[2] != "2" && base[3] != "3" && base[4] != "4" && base[5] != "5" && base[6] != "6" && base[7] != "7" && base[8] != "8" && base[9] != "9"){
+        return 0; //we have no winner
+    }
+    else 
+        return -1; //game is still in the progress
+}
 
+void drawBoard(){
+    system("cls");
+    printf("\n\n\t Tic Tac Toe \n\n");
+    printf("Plyer1 (X) - Player2 (O) \n\n\n");
+    printf("    |    |    \n");
+    printf(" %c  |   %c  |  %c  \n", base[1], base[2], base[3]);
+    printf("____|____|____\n");
+    printf("    |    |    \n");
+    printf(" %c  |   %c  |  %c  \n", base[4], base[5], base[6]);
+    printf("____|____|____\n");
+    printf("    |    |    \n");
+    printf(" %c  |   %c  |  %c  \n", base[7], base[8], base[9]);\
+    printf("    |    |    \n");
+}
